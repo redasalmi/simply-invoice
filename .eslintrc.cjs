e81762009e4d8537c4ea1db97c6bcc6167f3d7b3
redasalmi <reda.salmi.elt@gmail.com>
2023-06-21 22:31:06 +0100

    added eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+const ERROR = 2;
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:solid/typescript',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['solid', '@typescript-eslint'],
+  rules: {
+    'prefer-const': ERROR,
+    '@typescript-eslint/no-unused-vars': ERROR,
+  },
+};
