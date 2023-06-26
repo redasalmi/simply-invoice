ccf120dc76442f40f95b262eee341c96381dc695
redasalmi <reda.salmi.elt@gmail.com>
2023-06-26 01:10:02 +0100

    switched to an input key valud logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,17 @@
+import { createSignal, Show, For } from 'solid-js';
 import { createRouteAction } from 'solid-start';
 
 import type { FormData } from 'undici';
 
+type CompanyField = { key: string; value: string };
+
+const initData: CompanyField = { key: '', value: '' };
+
 export default function Home() {
+	const [showAddCompany, setShowAddCompay] = createSignal(false);
+	const [companyField, setCompanyField] = createSignal<CompanyField>(initData);
+	const [companyData, setCompanyData] = createSignal<CompanyField[]>([]);
+
 	const [{ pending }, { Form }] = createRouteAction(
 		async (formData: FormData) => {
 			const companyName = formData.get('company-name');
@@ -11,6 +20,22 @@ export default function Home() {
 			console.log({ companyName, clientName });
 		},
 	);
+
+	const toggleField = (event: MouseEvent, toggleShow: boolean) => {
+		event.preventDefault();
+		setShowAddCompay(toggleShow);
+
+		if (!toggleShow) {
+			setCompanyField(initData);
+		}
+	};
+
+	const AddCompanyField = (event: MouseEvent) => {
+		event.preventDefault();
+		setCompanyData((data) => [...data, companyField()]);
+		setShowAddCompay(false);
+		setCompanyField(initData);
+	};
 
 	return (
 		<div class="m-8">
@@ -28,6 +53,93 @@ export default function Home() {
 							name="company-name"
 							class="block rounded-md border-2 border-gray-500 px-4 py-2"
 						/>
+					</div>
+
+					<For each={companyData()}>
+						{(data) => {
+							return (
+								<div class="my-6">
+									<label for={data.key}>{data.key}</label>
+									<input
+										type="text"
+										id={data.key}
+										name={data.key}
+										value={data.value}
+										readOnly
+										class="block rounded-md border-2 border-gray-500 px-4 py-2"
+									/>
+								</div>
+							);
+						}}
+					</For>
+
+					<div class="my-6">
+						<Show when={showAddCompany()}>
+							<div class="my-6">
+								<label for="company-field-title">Field Title:</label>
+								<input
+									type="text"
+									id="company-field-title"
+									name="company-field-title"
+									value={companyField().key}
+									onInput={(e) =>
+										setCompanyField({
+											...companyField(),
+											key: e.currentTarget.value,
+										})
+									}
+									class="block rounded-md border-2 border-gray-500 px-4 py-2"
+								/>
+							</div>
+
+							<div class="my-6">
+								<label for="company-field-value">Field Value:</label>
+								<input
+									type="text"
+									id="company-field-value"
+									name="company-field-value"
+									value={companyField().value}
+									onInput={(e) =>
+										setCompanyField({
+											...companyField(),
+											value: e.currentTarget.value,
+										})
+									}
+									class="block rounded-md border-2 border-gray-500 px-4 py-2"
+								/>
+							</div>
+
+							<button
+								type="button"
+								class="my-6 rounded-md bg-blue-400 px-6 py-2"
+								onClick={AddCompanyField}
+							>
+								Add New Company Field
+							</button>
+						</Show>
+
+						<div>
+							<Show
+								when={showAddCompany()}
+								fallback={
+									<button
+										type="button"
+										class="rounded-md bg-blue-400 px-6 py-2"
+										onClick={(event) => toggleField(event, true)}
+									>
+										Show New Company Field
+									</button>
+								}
+							>
+								<button
+									type="button"
+									class="rounded-md bg-blue-400 px-6 py-2"
+									onClick={(event) => toggleField(event, false)}
+								>
+									Hide New Company Field
+								</button>
+							</Show>
+						</div>
 					</div>
 				</div>
 
