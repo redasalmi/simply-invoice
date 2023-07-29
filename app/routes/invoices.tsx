606a35353ccdf3ecd837826d36826c60f7238db5
redasalmi <reda.salmi.elt@gmail.com>
2023-07-29 17:26:24 +0100

    added new invoice route

--- app/routes/invoices.tsx
+++ app/routes/invoices.tsx
@@ -1,3 +1,30 @@
+import { json } from '@remix-run/node';
+import { useLoaderData, Link } from '@remix-run/react';
+
+import type { LoaderFunction } from '@remix-run/node';
+
+export const loader: LoaderFunction = () => {
+	return json([]);
+};
+
 export default function InvoicesRoutes() {
-	return <>Invoices Under construction... 🚧</>;
+	const invoices = useLoaderData<typeof loader>();
+
+	return (
+		<>
+			<Link to="/invoices/new" className="rounded-lg bg-blue-300 px-4 py-2">
+				Create New Invoice
+			</Link>
+			<div className="mt-6">
+				{invoices.length > 0 ? (
+					<>
+						Cool, you have {invoices.length} invoices
+						{invoices.length > 1 ? 's' : ''}
+					</>
+				) : (
+					<p>No invoices found.</p>
+				)}
+			</div>
+		</>
+	);
 }
---
+++ app/routes/invoices_.new.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { useFetcher } from '@remix-run/react';
+import { nanoid } from 'nanoid';
+
+import { FormField, Button, AddFormField } from '~/components';
+
+import type { Field } from '~/types';
+
+const pdfFormId = 'pdf-form';
+
+export default function NewInvoiceRoute() {
+	const fetcher = useFetcher();
+	const [fields, setFields] = React.useState<Field[]>([]);
+
+	React.useEffect(() => {
+		if (fetcher.state === 'idle' && fetcher.data) {
+			const link = document.createElement('a');
+			const fileName = 'invoice.pdf';
+			link.href = fetcher.data;
+			link.download = fileName;
+			link.click();
+		}
+	}, [fetcher.state, fetcher.data]);
+
+	return (
+		<>
+			<fetcher.Form action="/pdf" method="GET" id={pdfFormId}>
+				<h3 className="text-2xl">Company Data</h3>
+
+				<FormField name="company-name" label="Name" className="my-1" />
+				<FormField name="company-address" label="Address" className="my-1" />
+
+				{fields.map(({ name, label, defaultValue }) => (
+					<FormField
+						key={nanoid()}
+						name={name}
+						label={label}
+						defaultValue={defaultValue}
+						className="my-1"
+					/>
+				))}
+			</fetcher.Form>
+
+			<div className="my-2">
+				<AddFormField fieldPrefix="company" setFields={setFields} />
+			</div>
+
+			<div className="my-2">
+				<Button
+					form={pdfFormId}
+					type="submit"
+					text={
+						fetcher.state === 'submitting' ? '...Loading PDF' : 'Download PDF'
+					}
+				/>
+			</div>
+		</>
+	);
+}
