58b30ee5645531b0c47400dd1ec4fa1f4ce84c12
Reda Salmi <reda.salmi.elt@gmail.com>
2024-01-29 20:54:05 +0100

    adds basic invoice list table

--- app/components/ui/index.tsx
+++ app/components/ui/index.tsx
@@ -3,4 +3,5 @@ export * from './dialog';
 export * from './input';
 export * from './label';
 export * from './switch';
+export * from './table';
 export * from './tooltip';
--- app/routes/invoices.tsx
+++ app/routes/invoices.tsx
@@ -1,11 +1,19 @@
 import { useLoaderData, Link } from '@remix-run/react';
 import localforage from 'localforage';
 
-import { buttonVariants } from '~/components/ui';
+import {
+	buttonVariants,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '~/components/ui';
 import { invoicesKey } from '~/constants';
 import { cn } from '~/lib/utils';
 
-import type { Invoice } from '~/types';
+import type { Invoice, InvoiceField } from '~/types';
 
 export async function clientLoader() {
 	return {
@@ -13,26 +21,52 @@ export async function clientLoader() {
 	};
 }
 
+const formatter = new Intl.DateTimeFormat('en-GB', {
+	month: 'short',
+	day: '2-digit',
+	year: 'numeric',
+});
+
+const getEmail = (customer: Array<InvoiceField>) => {
+	return customer.find(({ label }) => label === 'email')?.value;
+};
+
 export default function InvoicesRoutes() {
 	const { invoices } = useLoaderData<typeof clientLoader>();
 
 	return (
 		<>
-			<Link
-				to="/invoices/new"
-				className={cn(
-					'rounded-lg bg-blue-300 px-4 py-2',
-					buttonVariants({ variant: 'default' }),
-				)}
-			>
-				Create New Invoice
-			</Link>
+			<div className="flex justify-end">
+				<Link
+					to="/invoices/new"
+					className={cn(
+						'rounded-lg bg-blue-300 px-4 py-2',
+						buttonVariants({ variant: 'default' }),
+					)}
+				>
+					Create New Invoice
+				</Link>
+			</div>
 			<div className="mt-6">
 				{invoices && invoices.length > 0 ? (
-					<p>
-						Cool, you have {invoices.length} invoice
-						{invoices.length > 1 ? 's' : ''}
-					</p>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>ID</TableHead>
+								<TableHead>Date</TableHead>
+								<TableHead>Email</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{invoices.map(({ id, createdAt, customer }) => (
+								<TableRow key={id}>
+									<TableCell>{id}</TableCell>
+									<TableCell>{formatter.format(new Date(createdAt))}</TableCell>
+									<TableCell>{getEmail(customer)}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
 				) : (
 					<p>No invoice found.</p>
 				)}
