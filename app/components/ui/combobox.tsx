c934974cc66caef8dfc3254adf42954de102aa40
Reda Salmi <reda.salmi.elt@gmail.com>
2024-02-19 16:41:39 +0100

    adds combobox component

---
+++ app/components/ui/combobox.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+
+import { Check, ChevronsUpDown } from 'lucide-react';
+
+import {
+	Button,
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '~/components/ui';
+
+import { cn } from '~/lib/utils';
+
+type ComboboxProps = {
+	list: Array<{ value: string; label: string }>;
+	label: string;
+	inputName: string;
+	inputPlaceholder: string;
+};
+
+export const Combobox = ({
+	label,
+	inputName,
+	inputPlaceholder,
+	list,
+}: ComboboxProps) => {
+	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = React.useState('');
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<input type="text" hidden readOnly name={inputName} value={value} />
+			<p>{label}</p>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					role="combobox"
+					aria-expanded={open}
+					className="w-[200px] justify-between"
+				>
+					{value
+						? list.find((listItem) => listItem.value === value)?.label
+						: inputPlaceholder}
+					<ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[200px] p-0">
+				<Command>
+					<CommandInput placeholder={inputPlaceholder} />
+					<CommandEmpty>No result found.</CommandEmpty>
+					<CommandGroup>
+						{list.map((listItem) => (
+							<CommandItem
+								key={listItem.value}
+								value={listItem.value}
+								onSelect={(currentValue) => {
+									setValue(currentValue === value ? '' : currentValue);
+									setOpen(false);
+								}}
+							>
+								<Check
+									className={cn(
+										'mr-2 h-4 w-4',
+										value === listItem.value ? 'opacity-100' : 'opacity-0',
+									)}
+								/>
+								{listItem.label}
+							</CommandItem>
+						))}
+					</CommandGroup>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	);
+};
