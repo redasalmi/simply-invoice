89acdf0ef9e4c421dd2e35e174b2325e8de95085
Reda Salmi <reda.salmi.elt@gmail.com>
2024-03-06 19:32:27 +0100

    add click away close to calendar

--- app/components/ui/date-picker.tsx
+++ app/components/ui/date-picker.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 
 import { Calendar as CalendarIcon } from 'lucide-react';
+import { useClickAway } from '~/hooks';
 
 import { Button } from '~/components/ui/button';
 import { Calendar } from '~/components/ui/calendar';
@@ -15,8 +16,12 @@ import {
 import { cn, dateFormatter } from '~/lib/utils';
 
 export function DatePicker() {
+	const datePickerRef = React.useRef<HTMLDivElement>(null);
+
 	const [open, setIsOpen] = React.useState(false);
 	const [date, setDate] = React.useState<Date>();
+
+	useClickAway(datePickerRef, () => setIsOpen(false));
 
 	const updateDate = (newDate?: Date) => {
 		if (newDate) {
@@ -26,45 +31,47 @@ export function DatePicker() {
 	};
 
 	return (
-		<Popover open={open}>
-			<PopoverTrigger asChild>
-				<Button
-					type="button"
-					onClick={() => setIsOpen(true)}
-					variant={'outline'}
-					className={cn(
-						'w-[280px] justify-start text-left font-normal',
-						!date && 'text-muted-foreground',
-					)}
-				>
-					{date ? (
-						<input
-							type="date"
-							hidden
-							readOnly
-							value={dateFormatter('en-US', {
-								month: '2-digit',
-								day: '2-digit',
-								year: 'numeric',
-							}).format(date)}
-						/>
-					) : null}
-					<CalendarIcon className="mr-2 h-4 w-4" />
-					{date ? (
-						dateFormatter('en-US').format(date)
-					) : (
-						<span>Pick a date</span>
-					)}
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-auto p-0">
-				<Calendar
-					mode="single"
-					selected={date}
-					onSelect={updateDate}
-					initialFocus
-				/>
-			</PopoverContent>
-		</Popover>
+		<div ref={datePickerRef}>
+			<Popover open={open}>
+				<PopoverTrigger asChild>
+					<Button
+						type="button"
+						onClick={() => setIsOpen(true)}
+						variant={'outline'}
+						className={cn(
+							'w-[280px] justify-start text-left font-normal',
+							!date && 'text-muted-foreground',
+						)}
+					>
+						{date ? (
+							<input
+								type="date"
+								hidden
+								readOnly
+								value={dateFormatter('en-US', {
+									month: '2-digit',
+									day: '2-digit',
+									year: 'numeric',
+								}).format(date)}
+							/>
+						) : null}
+						<CalendarIcon className="mr-2 h-4 w-4" />
+						{date ? (
+							dateFormatter('en-US').format(date)
+						) : (
+							<span>Pick a date</span>
+						)}
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent className="w-auto p-0">
+					<Calendar
+						mode="single"
+						selected={date}
+						onSelect={updateDate}
+						initialFocus
+					/>
+				</PopoverContent>
+			</Popover>
+		</div>
 	);
 }
