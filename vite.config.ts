b9ee304dfdc542f6f2331f9ce2967f6a91fca52c
Reda Salmi <reda.salmi.elt@gmail.com>
2024-03-11 18:28:33 +0100

    adds remix devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 import { vitePlugin as remix } from '@remix-run/dev';
+import { remixDevTools } from 'remix-development-tools';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+	plugins: [remixDevTools(), remix(), netlifyPlugin(), tsconfigPaths()],
 });
