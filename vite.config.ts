37f4d4383efeef6663c01385cc35abd3f57551c3
Reda Salmi <reda.salmi.elt@gmail.com>
2024-05-06 14:30:23 +0100

    removes remix devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 import { vitePlugin as remix } from '@remix-run/dev';
-import { remixDevTools } from 'remix-development-tools';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	plugins: [
-		remixDevTools(),
 		remix({
 			future: {
 				unstable_singleFetch: true,
