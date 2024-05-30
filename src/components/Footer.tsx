128b3d6f58eb9f1948d5af3bdbcb5be3f5d89693
Reda Salmi <reda.salmi.elt@gmail.com>
2024-05-30 13:43:57 +0100

    bye again solid

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-	return (
-		<footer class="bg-primary text-primary-foreground py-6 font-bold">
-			<div class="container mx-auto">
-				<p>Just a footer lol</p>
-			</div>
-		</footer>
-	);
-};
