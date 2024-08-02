5aa9cee1cd95137413f53e7b672d91f29a60d92b
Reda Salmi <reda.salmi.elt@gmail.com>
2024-08-02 21:02:35 +0100

    updates feature flags

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
 	plugins: [
 		remix({
 			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
 				unstable_singleFetch: true,
 				unstable_lazyRouteDiscovery: true,
 			},
