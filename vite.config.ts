21ce401c545a7bac1fa65fe8226d55455a117f43
Reda Salmi <reda.salmi.elt@gmail.com>
2024-09-10 08:54:18 +0100

    dependencies upgrade and enables deps optimization

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
 				v3_relativeSplatPath: true,
 				v3_throwAbortReason: true,
 				unstable_lazyRouteDiscovery: true,
+				unstable_optimizeDeps: true,
+				unstable_singleFetch: true,
 			},
 		}),
 		netlifyPlugin(),
