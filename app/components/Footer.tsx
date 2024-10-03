0b2526f1437c3a9697e51502afb38ca6ac1bcbed
Reda Salmi <reda.salmi.elt@gmail.com>
2024-10-03 21:42:38 +0100

    deleted everything

--- app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-	return (
-		<footer className="py-6 font-bold">
-			<div className="container mx-auto">
-				<p>Just a footer lol</p>
-			</div>
-		</footer>
-	);
-}
--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
-	plugins: [
-		require('tailwindcss'),
-		require('autoprefixer'),
-		require('postcss-preset-env'),
-		require('cssnano'),
-	],
-};
