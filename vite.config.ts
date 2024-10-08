b1209ab8191726b71ae32089609122833d42051a
Reda Salmi <reda.salmi.elt@gmail.com>
2024-10-08 19:11:16 +0100

    adds react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,26 @@ import tailwindcss from '@tailwindcss/vite';
 import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [
+		tailwindcss(),
+		reactRouter(),
+		tsconfigPaths(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			babelConfig: {
+				presets: ['@babel/preset-typescript'],
+				plugins: [
+					[
+						'babel-plugin-react-compiler',
+						{
+							target: '18',
+						},
+					],
+				],
+			},
+		}),
+	],
 });
