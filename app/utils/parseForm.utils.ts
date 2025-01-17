61f7d50a67ed361e40e702a032d9aa93f07aab29
redasalmi <reda.salmi.elt@gmail.com>
2025-01-17 20:05:19 +0100

    adds db type and adds a removeEmptyProperties that is used before parsing form data

--- app/utils/parseForm.utils.ts
+++ app/utils/parseForm.utils.ts
@@ -1,9 +1,23 @@
 import * as v from 'valibot';
 
+type FormDataEntriesObject = Record<string, FormDataEntryValue>;
+
+function removeEmptyProperties(obj: FormDataEntriesObject) {
+	for (const key in obj) {
+		if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
+			delete obj[key];
+		} else if (typeof obj[key] === 'object') {
+			removeEmptyProperties(obj[key] as unknown as FormDataEntriesObject);
+		}
+	}
+
+	return obj;
+}
+
 export function parseFormData<
 	T extends v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>,
 >(formData: FormData, schema: T) {
-	const object = Object.fromEntries(formData);
+	const object = removeEmptyProperties(Object.fromEntries(formData));
 	const data = v.safeParse(schema, object, { abortPipeEarly: true });
 
 	if (data.issues) {
