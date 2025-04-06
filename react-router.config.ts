e9ba29086f4f7961dc4dee9448c6a4ab65308689
redasalmi <reda.salmi.elt@gmail.com>
2025-04-06 17:24:16 +0100

    upgrades packages and disables unstable features because its shit

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,10 +2,4 @@ import type { Config } from '@react-router/dev/config';
 
 export default {
 	ssr: false,
-	future: {
-		unstable_optimizeDeps: true,
-		unstable_splitRouteModules: true,
-		unstable_viteEnvironmentApi: true,
-		unstable_middleware: true,
-	},
 } satisfies Config;
