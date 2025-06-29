647175ac72e4091fca1f5fb541eb3b87a378ccef
redasalmi <reda.salmi.elt@gmail.com>
2025-06-29 17:01:35 +0100

    adds customers detail route

--- src/renderer/src/routes.tsx
+++ src/renderer/src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { RootRoute } from "~/renderer/root";
 import { companiesRoutes } from "~/renderer/routes/companies/companies-routes";
+import { customersRoutes } from "~/renderer/routes/customers/customers-routes";
 import { HomeRoute } from "~/renderer/routes/home";
 import { taxesRoutes } from "~/renderer/routes/taxes/taxes-routes";
 
@@ -13,6 +14,7 @@ export const router = createBrowserRouter([
 				Component: HomeRoute,
 			},
 			...companiesRoutes,
+			...customersRoutes,
 			...taxesRoutes,
 		],
 	},
--- src/renderer/src/routes/customers/customers-routes.tsx
+++ src/renderer/src/routes/customers/customers-routes.tsx
@@ -1,4 +1,5 @@
 import type { RouteObject } from "react-router";
+import { CustomerDetailRoute, customerDetailLoader } from "./customer-detail";
 import { CustomersListRoute, customersListLoader } from "./customers-list";
 
 export const customersRoutes: RouteObject[] = [
@@ -6,5 +7,12 @@ export const customersRoutes: RouteObject[] = [
 		path: "/customers",
 		loader: customersListLoader,
 		Component: CustomersListRoute,
+		children: [
+			{
+				path: "detail/:customerId",
+				loader: customerDetailLoader,
+				Component: CustomerDetailRoute,
+			},
+		],
 	},
 ];
