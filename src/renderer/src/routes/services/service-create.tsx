9095840417bafc7b491ac501c290880ea7ca78dc
redasalmi <reda.salmi.elt@gmail.com>
2025-07-05 15:49:07 +0100

    adds services create route

---
+++ src/renderer/src/routes/services/service-create.tsx
@@ -0,0 +1,60 @@
+import {
+	type ActionFunctionArgs,
+	Form,
+	redirect,
+	useActionData,
+	useNavigation,
+} from "react-router";
+import { Button } from "~/renderer/components/ui/button";
+import { FormField } from "~/renderer/components/ui/form-field";
+import type { CreateServiceInput } from "~/types";
+
+export async function serviceCreateAction({ request }: ActionFunctionArgs) {
+	const formData = await request.formData();
+	const service = Object.fromEntries(formData) as unknown as CreateServiceInput;
+	const result = await window.api.db.services.create(service);
+
+	if ("errors" in result) {
+		return {
+			errors: result.errors,
+		};
+	}
+
+	return redirect("/services");
+}
+
+export function ServiceCreateRoute() {
+	const actionData = useActionData<typeof serviceCreateAction>();
+
+	const navigation = useNavigation();
+	const isLoading = navigation.state !== "idle";
+	const isSubmitting = navigation.state === "submitting";
+
+	return (
+		<section>
+			<Form className="flex flex-col gap-4" method="post">
+				<FormField errors={actionData?.errors?.nested?.name}>
+					<FormField.Label>Name</FormField.Label>
+					<FormField.Input name="name" type="text" />
+					<FormField.ErrorMessage />
+				</FormField>
+
+				<FormField errors={actionData?.errors?.nested?.description}>
+					<FormField.Label>Description</FormField.Label>
+					<FormField.Input name="description" type="text" />
+					<FormField.ErrorMessage />
+				</FormField>
+
+				<FormField errors={actionData?.errors?.nested?.rate}>
+					<FormField.Label>Rate(%)</FormField.Label>
+					<FormField.NumberInput name="rate" />
+					<FormField.ErrorMessage />
+				</FormField>
+
+				<Button disabled={isSubmitting} type="submit">
+					{isLoading ? "Saving Service..." : "Save Service"}
+				</Button>
+			</Form>
+		</section>
+	);
+}
--- src/renderer/src/routes/services/services-routes.tsx
+++ src/renderer/src/routes/services/services-routes.tsx
@@ -1,5 +1,9 @@
 import type { RouteObject } from "react-router";
 import {
+	ServiceCreateRoute,
+	serviceCreateAction,
+} from "~/renderer/routes/services/service-create";
+import {
 	ServicesListRoute,
 	servicesListLoader,
 } from "~/renderer/routes/services/services-list";
@@ -9,5 +13,10 @@ export const servicesRoutes: RouteObject[] = [
 		path: "/services",
 		loader: servicesListLoader,
 		Component: ServicesListRoute,
+	},
+	{
+		path: "/services/create",
+		action: serviceCreateAction,
+		Component: ServiceCreateRoute,
 	},
 ];
